9b5284a52cff17af8c4658d60fd84d004d15365f
Noah Yona <noahyona@Noahs-MacBook-Air.local>
2024-03-13 20:44:26 -0700

    hash routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import VisionPage from './pages/VisionPage'
 import ServicesPage from './pages/ServicesPage'
@@ -8,14 +8,14 @@ import ContactPage from './pages/ContactPage'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/vision" element={<VisionPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
